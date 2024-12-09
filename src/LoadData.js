c123012a44d900b7328dd59685d6c3f978e0184a
svdiqhayatuddeen <svdiqhayatuddeen@gmail.com>
2024-12-09 10:45:45 +0000

    added long data and editted the loadData() function

--- src/LoadData.js
+++ src/LoadData.js
@@ -2,11 +2,32 @@
 
 import fs from "fs"
 
-export const LoadData = ( code ) => {
+// loads data for shot format
+export const LoadDataShort = ( code ) => {
   const code_number = String(code)
     // 
     try {
-        const data = fs.readFileSync( `./assets/local_cache/${code_number}-data-short.json`, "utf8" )
+        const data = fs.readFileSync( `./assets/local_cache_short/${code_number}-data-short.json`, "utf8" )
+        const json = JSON.parse( data )
+      // return the the data
+      return json
+      
+    } catch (error) {
+      //return a json error
+      return {
+        code,
+        error: "File missing",
+        message: "Status code in UNASSIGNED"
+      }
+    }
+}
+
+// loads data for long format
+export const LoadDataLong = ( code ) => {
+  const code_number = String(code)
+    // 
+    try {
+        const data = fs.readFileSync( `./assets/local_cache_long/${code_number}-data-long.json`, "utf8" )
         const json = JSON.parse( data )
       // return the the data
       return json
